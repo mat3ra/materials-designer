f40af17ae8823061a75f9d1158820e242198e3a9
VsevolodX <vsevolodbiryukov@icloud.com>
2025-02-18 14:24:44 -0800

    update: adjsut step def

--- tests/cypress/support/step_definitions/jupyterlite/I enter following text into input.ts
+++ tests/cypress/support/step_definitions/jupyterlite/I enter following text into input.ts
@@ -1,6 +1,13 @@
-import { When } from "@badeball/cypress-cucumber-preprocessor";
+import { DataTable, When } from "@badeball/cypress-cucumber-preprocessor";
+import { parseTable } from "@mat3ra/tede/src/js/cypress/utils/table";
+
 import MaterialDesignerPage from "../../widgets/MaterialDesignerPage";
 
-When("I enter the following text into input", (text: string) => {
+interface TextInput {
+    text: string;
+}
+
+When("I enter following text into input", (table: DataTable) => {
+    const [{ text }] = parseTable<TextInput>(table);
     new MaterialDesignerPage().designerWidget.jupyterLiteSession.enterTextIntoInput(text);
 });
