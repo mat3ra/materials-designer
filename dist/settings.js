3b1d9b1a648efbf49a23e1a3d018bc55f7139284
VsevolodX <vsevolodbiryukov@icloud.com>
2025-08-08 21:17:09 -0700

    tmp: set dev url

--- dist/settings.js
+++ dist/settings.js
@@ -1,7 +1,7 @@
 import { DarkMaterialUITheme } from "@exabyte-io/cove.js/dist/theme";
 export const theme = DarkMaterialUITheme;
 // get from https://github.com/Exabyte-io/jupyterlite/pull/N
-const JUPYTERLITE_DEVELOPMENT_URL = "https://deploy-preview-N--mat3ra-jupyterlite.netlify.app";
+const JUPYTERLITE_DEVELOPMENT_URL = "https://deploy-preview-56--mat3ra-jupyterlite.netlify.app";
 export const ORIGIN_URL = import.meta.env.USE_JUPYTERLITE_DEV_URL
     ? JUPYTERLITE_DEVELOPMENT_URL
     : undefined;
