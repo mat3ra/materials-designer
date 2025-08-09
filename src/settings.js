916c985a83ebd5f97b172bace2d5a6ce99a12cb7
VsevolodX <vsevolodbiryukov@icloud.com>
2025-08-08 21:22:55 -0700

    update: use vite

--- src/settings.js
+++ src/settings.js
@@ -5,6 +5,6 @@ export const theme = DarkMaterialUITheme;
 // get from https://github.com/Exabyte-io/jupyterlite/pull/N
 const JUPYTERLITE_DEVELOPMENT_URL = "https://deploy-preview-56--mat3ra-jupyterlite.netlify.app";
 
-export const ORIGIN_URL = import.meta.env.USE_JUPYTERLITE_DEV_URL
+export const ORIGIN_URL = import.meta.env.VITE_USE_JUPYTERLITE_DEV_URL
     ? JUPYTERLITE_DEVELOPMENT_URL
     : undefined;
