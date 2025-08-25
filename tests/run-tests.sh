64bd250959328738e9afa4380871f37567185972
VsevolodX <vsevolodbiryukov@icloud.com>
2025-08-24 19:50:06 -0700

    try: dont run hc

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Read from environment variable (set in .env file)
-USE_DEV_URL=$VITE_USE_JUPYTERLITE_DEV_URL
-
-# Set default TAGS based on config
-if [ "$USE_DEV_URL" = "true" ]; then
-    TAGS=${TAGS:-"not @ignore"}
+if [ "$VITE_USE_JUPYTERLITE_DEV_URL" = "true" ]; then
+    TAGS=${TAGS:-"not @ignore and @notebook_healthcheck"}
 else
     TAGS=${TAGS:-"not @ignore and not @notebook_healthcheck"}
 fi
