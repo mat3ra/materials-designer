53ee515762a9eecb62a219bf7655ea342a29cf5b
VsevolodX <vsevolodbiryukov@icloud.com>
2025-08-24 18:27:25 -0700

    chore: move url to env

--- dist/settings.d.ts
+++ dist/settings.d.ts
@@ -1,2 +1,2 @@
 export const theme: import("@mui/material").Theme;
-export const JUPYTERLITE_ORIGIN_URL: "https://deploy-preview-58--mat3ra-jupyterlite.netlify.app" | undefined;
+export const JUPYTERLITE_ORIGIN_URL: any;
--- dist/settings.js
+++ dist/settings.js
@@ -1,7 +1,6 @@
 import { DarkMaterialUITheme } from "@exabyte-io/cove.js/dist/theme";
 export const theme = DarkMaterialUITheme;
-// get the number for PR N from  https://github.com/Exabyte-io/jupyterlite/pull/N
-const JUPYTERLITE_DEVELOPMENT_URL = "https://deploy-preview-58--mat3ra-jupyterlite.netlify.app";
+const JUPYTERLITE_DEVELOPMENT_URL = import.meta.env.VITE_JUPYTERLITE_DEVELOPMENT_URL;
 export const JUPYTERLITE_ORIGIN_URL = import.meta.env.VITE_USE_JUPYTERLITE_DEV_URL === "true"
     ? JUPYTERLITE_DEVELOPMENT_URL
     : undefined; // if not set, will use the default URL
