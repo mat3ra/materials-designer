9e006b9d31286544da29c33fcd167a94bcebd9db
VsevolodX <vsevolodbiryukov@icloud.com>
2025-12-01 09:44:13 -0800

    try: process env

--- src/settings.js
+++ src/settings.js
@@ -1,9 +1,6 @@
 import { DarkMaterialUITheme } from "@exabyte-io/cove.js/dist/theme";
 
 export const theme = DarkMaterialUITheme;
-
-const JUPYTERLITE_DEVELOPMENT_URL = import.meta.env.VITE_JUPYTERLITE_DEVELOPMENT_URL;
+const JUPYTERLITE_DEVELOPMENT_URL = process.env.VITE_JUPYTERLITE_DEVELOPMENT_URL;
 export const JUPYTERLITE_ORIGIN_URL =
-    import.meta.env.VITE_USE_JUPYTERLITE_DEV_URL === "true"
-        ? JUPYTERLITE_DEVELOPMENT_URL
-        : undefined; // if not set, will use the default URL
+    process.env.VITE_USE_JUPYTERLITE_DEV_URL === "true" ? JUPYTERLITE_DEVELOPMENT_URL : undefined; // if not set, will use the default URL
