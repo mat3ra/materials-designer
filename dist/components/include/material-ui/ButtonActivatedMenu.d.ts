ae0755bd8c9c139a0686bfa836140c09b54700fd
VsevolodX <vsevolodbiryukov@icloud.com>
2026-05-15 10:38:57 -0700

    update: fix labels for comb set

--- dist/components/include/material-ui/ButtonActivatedMenu.d.ts
+++ dist/components/include/material-ui/ButtonActivatedMenu.d.ts
@@ -10,19 +10,19 @@ export class ButtonActivatedMenuMaterialUI extends React.Component<any, any, any
 }
 export namespace ButtonActivatedMenuMaterialUI {
     namespace propTypes {
-        let title: PropTypes.Requireable<string>;
-        let id: PropTypes.Requireable<string>;
-        let isOpen: PropTypes.Requireable<boolean>;
-        let children: PropTypes.Requireable<PropTypes.ReactNodeLike>;
+        const title: PropTypes.Requireable<string>;
+        const id: PropTypes.Requireable<string>;
+        const isOpen: PropTypes.Requireable<boolean>;
+        const children: PropTypes.Requireable<PropTypes.ReactNodeLike>;
     }
     namespace defaultProps {
-        let id_1: string;
+        const id_1: string;
         export { id_1 as id };
-        let isOpen_1: boolean;
+        const isOpen_1: boolean;
         export { isOpen_1 as isOpen };
-        let children_1: undefined;
+        const children_1: undefined;
         export { children_1 as children };
-        let title_1: string;
+        const title_1: string;
         export { title_1 as title };
     }
 }
